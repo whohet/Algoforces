c5ce1fa24164849c927a6b506820f5e88a02018b
harshil-270 <harshiltagadiya@gmail.com>
2021-09-29 00:00:42 +0530

    using variable instead of string

--- client/src/api/userAuth.js
+++ client/src/api/userAuth.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { SERVER_URL } from "../config/config";
+import { CLIENT_URL, SERVER_URL } from "../config/config";
 
 export const loginAPI = async (userData) => {
   const res = await axios.post(`${SERVER_URL}/users/login`, userData, { withCredentials: true });
@@ -15,7 +15,7 @@ export const isLoggedInAPI = async () => {
   const res = await axios.get(`${SERVER_URL}/users/isLoggedIn`, {
     withCredentials: true,
     headers: {
-      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Origin": CLIENT_URL,
       "Access-Control-Allow-Credentials": true,
       "Content-Type": "application/json",
     },
