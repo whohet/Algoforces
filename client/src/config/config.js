1579f9e9d4977baa0aa131a44f919b0e1c0ec551
harshil-270 <harshiltagadiya@gmail.com>
2021-09-29 21:21:18 +0530

    replaced client and server url with env variables

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -4,8 +4,8 @@ const env = process.env.NODE_ENV || "development";
 
 if (env === "production") {
   // Set deployed site's client and server url here
-  CLIENT_URL = "";
-  SERVER_URL = "";
+  CLIENT_URL = process.env.REACT_APP_CLIENT_URL;
+  SERVER_URL = process.env.REACT_APP_SERVER_URL;
 } else {
   CLIENT_URL = "http://localhost:3000";
   SERVER_URL = "http://localhost:5000";
--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
-let CLIENT_URL = '';
-let SERVER_URL = '';
-const env = process.env.NODE_ENV || 'development';
+let CLIENT_URL = "";
+let SERVER_URL = "";
+const env = process.env.NODE_ENV || "development";
 
-if (env == 'production') {
+if (env == "production") {
   // Set deployed site's client and server url here
-  CLIENT_URL = ''; 
-  SERVER_URL = '';
+  CLIENT_URL = process.env.CLIENT_URL;
+  SERVER_URL = process.env.SERVER_URL;
 } else {
-  CLIENT_URL = 'http://localhost:3000';
-  SERVER_URL = 'http://localhost:5000';
+  CLIENT_URL = "http://localhost:3000";
+  SERVER_URL = "http://localhost:5000";
 }
 
-module.exports = {CLIENT_URL, SERVER_URL};
+module.exports = { CLIENT_URL, SERVER_URL };
