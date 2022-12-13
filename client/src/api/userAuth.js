0d94cc5c9b96623a7dc22765446820ef9ad059f1
harshil-270 <harshiltagadiya@gmail.com>
2022-12-13 20:07:25 +0530

    production cookie issue fix #5

--- client/src/api/userAuth.js
+++ client/src/api/userAuth.js
@@ -4,11 +4,6 @@ import { CLIENT_URL, SERVER_URL } from "../config/config";
 export const loginAPI = async (userData) => {
   const res = await axios.post(`${SERVER_URL}/users/login`, userData, {
     withCredentials: true,
-    headers: {
-      "Access-Control-Allow-Origin": CLIENT_URL,
-      "Access-Control-Allow-Credentials": true,
-      "Content-Type": "application/json",
-    },
   });
   return res.data;
 };
@@ -23,11 +18,11 @@ export const registerAPI = async (userData) => {
 export const isLoggedInAPI = async () => {
   const res = await axios.get(`${SERVER_URL}/users/isLoggedIn`, {
     withCredentials: true,
-    headers: {
-      "Access-Control-Allow-Origin": CLIENT_URL,
-      "Access-Control-Allow-Credentials": true,
-      "Content-Type": "application/json",
-    },
+    // headers: {
+    //   "Access-Control-Allow-Origin": CLIENT_URL,
+    //   "Access-Control-Allow-Credentials": true,
+    //   "Content-Type": "application/json",
+    // },
   });
   return res.data;
 };
